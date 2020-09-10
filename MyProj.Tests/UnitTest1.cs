64182eb0170a7be4e899cd77a2dc3eb5084072de
Ghosh <mohua.ghosh@accenture.com>
2020-09-10 18:19:14 +0530

    initial commit

---
+++ MyProj.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using MyProj;
+namespace MyProj.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
---
+++ MyProj/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+ 
+namespace MyProject
+{
+    public class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
